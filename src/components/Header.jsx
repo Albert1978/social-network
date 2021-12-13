b5f5da2e3fa99e910b538ef0f0bddd6793c4cab5
Albert <bert1978@rambler.ru>
2021-12-13 13:03:26 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+	return (
+		<header className='header'>
+			<img src='https://lh3.googleusercontent.com/proxy/CLCwQWbu2YvRrBzqzzXO2ASWyHZvEUYLR9WRBX5sBCj22Wm8Vy9bRVOzJF-lYR0t3H232JDFaklOGAjsuIymFR1HUuEZdIazljymaOm8CfQIpssFeX5eEg' />
+		</header>
+	)
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+const Profile = () => {
+	return (
+		<div className='content'>
+			<div>
+				<img src='https://pbs.twimg.com/media/FFWoVyqX0AU31qj?format=jpg&name=4096x4096' />
+			</div>
+			<div>
+				ava + description
+			</div>
+			<div>
+				My posts
+				<div>
+					New post
+				</div>
+				<div>
+					<div>post1</div>
+					<div>post2</div>
+				</div>
+			</div>
+
+		</div>
+	)
+}
+
+export default Profile;
